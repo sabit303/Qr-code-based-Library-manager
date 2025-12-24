ebb277806c58bd937160e3e259aa0879c1a5e6f2
sabit <arkskhan34@gmail.com>
2025-12-24 23:59:52 +0600

    Refactor authController to include token authentication and improve error handling

--- api/src/Controller/authController.ts
+++ api/src/Controller/authController.ts
@@ -1,11 +1,15 @@
-import { Request,Response } from "express";
+import { Request,Response,NextFunction } from "express";
 import { loginDTO } from "../DTOs/LoginDTO.js";
 import { loginServices } from "../Services/LoginService.js";
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 export class authController{
 
         constructor(private LoginService: loginServices){}
+
     async  login(req:Request,res: Response):Promise<Response> {
         const dto: loginDTO = req.body;
         const result = this.LoginService.validateStudent(dto.email, dto.password);
@@ -18,5 +22,19 @@ export class authController{
                 msg: "User Not Exist"
             })
         
+    }
+
+    async auth(req: Request,res:Response,next: NextFunction){
+        const token = req.headers.authorization?.split(" ")[1];
+
+        try{
+            if (!token || !process.env.jwt_secret) {
+                return res.status(401).json({ msg: "Unauthorized" });
+            }
+            const decoded = jwt.verify(token, process.env.jwt_secret);
+            next();
+        } catch (error) {
+            return res.status(401).json({ msg: "Invalid token" });
+        }
     }
 }
