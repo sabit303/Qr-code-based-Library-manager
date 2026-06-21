428b9eac0b9e7632463519efd14e2679c350a324
sabit <arkskhan34@gmail.com>
2026-06-21 16:58:53 +0600

    CORS error fix

--- api/src/app.ts
+++ api/src/app.ts
@@ -11,9 +11,9 @@ const app: Application = express();
 
 // Custom CORS middleware to allow requests from Flutter Web
 app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', '*');
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
